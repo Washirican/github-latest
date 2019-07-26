dbf684ea666c01c4cb9ad45223a1d75ebbc8349b
Washirican <danrod@uw.edu>
2019-07-25 22:39:32 -0700

    Lesson 6 activity final submit

--- main.py
+++ main.py
@@ -9,12 +9,8 @@ import requests
 if __name__ == "__main__":
     username = sys.argv[1]
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
+    events = requests.get('https://api.github.com/users/{}/events'.format(username))
+    event_type = events.json()[0]['type']
+    created_at = events.json()[0]['created_at']
 
-    print("COMPLETE THE TODOs")
-    
-
-
+    print('GitHub user {} executed a {} on {}'.format(username, event_type, created_at))
